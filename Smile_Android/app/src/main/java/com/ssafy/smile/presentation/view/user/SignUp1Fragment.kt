38bae47753fe294d3498fb99fdcddad2a6057798
minha721 <m0721@naver.com>
2023-01-17 17:10:47 +0900

    [FEAT] #1 - 회원가입 #42 회원가입1, 2 - 뷰 구현
    Former-commit-id: 22c00fa067010113889e13cf5d89f1250aca77a4

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/SignUp1Fragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/SignUp1Fragment.kt
@@ -1,6 +1,24 @@
 package com.ssafy.smile.presentation.view.user
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.ssafy.smile.MainActivity
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentSignUp1Binding
+import com.ssafy.smile.presentation.base.BaseFragment
 
-class SignUp1Fragment : Fragment() {
+
+class SignUp1Fragment : BaseFragment<FragmentSignUp1Binding>(FragmentSignUp1Binding::bind, R.layout.fragment_sign_up1) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as MainActivity).setToolBar(true, true, "회원가입")
+
+        binding.apply {
+            btnNext.setOnClickListener {
+                findNavController().navigate(R.id.action_signUp1Fragment_to_signUp2Fragment)
+            }
+        }
+    }
 }
--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/SignUp2Fragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/SignUp2Fragment.kt
@@ -1,6 +1,23 @@
 package com.ssafy.smile.presentation.view.user
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.ssafy.smile.MainActivity
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentSignUp2Binding
+import com.ssafy.smile.presentation.base.BaseFragment
 
-class SignUp2Fragment : Fragment() {
+class SignUp2Fragment : BaseFragment<FragmentSignUp2Binding>(FragmentSignUp2Binding::bind, R.layout.fragment_sign_up2) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as MainActivity).setToolBar(true, true, "회원가입")
+
+        binding.apply {
+            btnJoin.setOnClickListener {
+                // TODO : 바로 로그인
+            }
+        }
+    }
 }
