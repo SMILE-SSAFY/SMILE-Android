a53a97c46a93da9e5af8331307c5e3b0ff8fc9b0
서재건 <sjk1052005@gmail.com>
2023-01-18 22:26:04 +0900

    [REFACTOR] #1 - Change to @Data
    - @getter, setter, tostring을 @Data로 변환
    
    
    Former-commit-id: f9649f28ca89204d549cf2135729be66b621a736

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/RegisterFormDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/RegisterFormDto.java
@@ -2,14 +2,10 @@ package com.ssafy.api.dto;
 
 import com.sun.istack.NotNull;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
 @Builder
-@Getter
-@Setter
-@ToString
+@Data
 public class RegisterFormDto {
     @NotNull
     private String email;
