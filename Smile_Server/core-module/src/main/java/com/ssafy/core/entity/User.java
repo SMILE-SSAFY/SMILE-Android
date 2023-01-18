5c24b617dc8b47f18b7d2a79797b8813028c1840
서재건 <sjk1052005@gmail.com>
2023-01-18 19:47:55 +0900

    [ADD] #1 - Add User Entity
    - implements UserDetails
    - 유저 컬럼 : id, email, password, name, nickname, phoneNumber, role
    - 테이블 이름 지정 "member"와 "phone_number" 실행 문제는 없으나 빨간 경고 등 표시
    
    
    Former-commit-id: f6483010380771345a465a98d4b3723e935099c3

--- Smile_Server/core-module/src/main/java/com/ssafy/core/entity/User.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/entity/User.java
@@ -1,23 +1,106 @@
 package com.ssafy.core.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.core.code.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
 
 
 @Entity
-@Table(name = "users")
+@Builder
 @Getter
+@Setter
+@ToString
 @NoArgsConstructor
-public class User {
+@AllArgsConstructor
+@Table(name = "member")
+public class User implements UserDetails {
+// FIX: "member" 부분에 unresolved database references in annotations 빨간 경고 있으나 실행은 이상 없음
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    // FIX: "phone_number" unresolved database references in annotations 빨간 경고 있으나 실행은 이상 없음
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(Role.USER.getName()));
+        return authorities;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+
+    @Override
+    public String getPassword(){
+        return this.password;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
+
