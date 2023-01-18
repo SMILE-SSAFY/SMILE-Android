c6b159da4b092c42206ed2a852fc15744e3fc27f
jiy00nlee <sundaeluv98@gmail.com>
2023-01-18 12:54:38 +0900

    [FEAT] #14 - Android 게시글 UI 그리기
    Git issue : #S08P12D102-137
    
    
    Former-commit-id: b327459d49010b070241cd9e26a5ea644000d552

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/WritePostFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/WritePostFragment.kt
@@ -1,6 +1,32 @@
 package com.ssafy.smile.presentation.view.portfolio
 
-import androidx.fragment.app.Fragment
 
-class WritePostFragment : Fragment() {
+import android.widget.ArrayAdapter
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentWritePostBinding
+import com.ssafy.smile.presentation.base.BaseFragment
+
+class WritePostFragment : BaseFragment<FragmentWritePostBinding>(FragmentWritePostBinding::bind, R.layout.fragment_write_post) {
+    private val spinnerAdapter : ArrayAdapter<String> by lazy {
+        val items = resources.getStringArray(R.array.spinner_category)
+        ArrayAdapter(requireContext(), R.layout.item_spinner_category, items)
+    }
+
+    override fun initView() {
+        mainActivity.setToolBar(isUsed = true, isBackUsed = true, title = "게시글 업로드")
+        setSpinnerAdapter()
+    }
+
+    override fun setEvent() {
+
+    }
+
+    private fun setSpinnerAdapter(){
+       binding.apply {
+           tvCategoryContent.setAdapter(spinnerAdapter)
+           tvCategoryContent.setOnItemClickListener { _, _, _, _ ->
+               showToast(requireContext(), tvCategoryContent.text.toString())
+           }
+       }
+    }
 }
