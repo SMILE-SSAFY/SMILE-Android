5de2f703b8a65f396a98c3bdaf4c0c63922c6bc6
서재건 <sjk1052005@gmail.com>
2023-01-18 17:49:14 +0900

    [ADD] #1 - Add RegisterFormDto
    - 이메일, 패스워드, 이름, 닉네임, 폰넘버
    
    
    Former-commit-id: d0ec9b4bc2df0e90c92694bdca612b305130625a

---
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/RegisterFormDto.java
@@ -0,0 +1,33 @@
+package com.ssafy.api.dto;
+
+import com.sun.istack.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+public class RegisterFormDto {
+    @NotNull
+    private String email;
+    @NotNull
+    private String password;
+    @NotNull
+    private String name;
+    @NotNull
+    private String nickname;
+    @NotNull
+    private String phoneNumber;
+
+    public RegisterFormDto(String email, String password, String name, String nickname, String phoneNumber) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
