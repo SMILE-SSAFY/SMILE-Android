fdb766108375dbd612c71829c51728d8fc6bd270
jungeun <jegarnet23@gmail.com>
2023-01-18 15:35:03 +0900

    [ADD] #9 - Add function entity to dto
    [Resolves] #59
    
    
    Former-commit-id: 2f52bd5cbba42dbe791575bac67d416d4e8d5664

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/PhotographerDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/PhotographerDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.api.dto;
 
+import com.ssafy.core.entity.Photographer;
 import com.ssafy.core.entity.Places;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,4 +20,15 @@ public class PhotographerDto {
     private String account;
     private int heart;
     private List<Places> places;
+
+    public PhotographerDto of(Photographer photographer) {
+        return PhotographerDto.builder()
+                .photographerIdx(photographer.getId())
+                .profileImg(photographer.getProfileImg())
+                .introduction(photographer.getIntroduction())
+                .account(photographer.getAccount())
+                .heart(photographer.getHeart())
+                .places(photographer.getPlaces())
+                .build();
+    }
 }
