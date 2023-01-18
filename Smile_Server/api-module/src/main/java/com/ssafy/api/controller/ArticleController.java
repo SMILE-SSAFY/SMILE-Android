c8622d3dd606d53e8452b74d08f4ef061c8d3ebb
RingCho <marsm4rs@gmail.com>
2023-01-18 16:32:29 +0900

    [FEAT] # 13,14 ARTICLE GET DETAIL, DELETE API
    - User 인증 정보 넣는 거 조금 더 찾아 봐야 할 듯
    -
    
    
    Former-commit-id: c19d74f05906f96d75c96aa8eb00887bfc1b72b1

--- Smile_Server/api-module/src/main/java/com/ssafy/api/controller/ArticleController.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/controller/ArticleController.java
@@ -1,11 +1,15 @@
 package com.ssafy.api.controller;
 
 import com.ssafy.api.dto.ArticleDetailDto;
+import com.ssafy.api.dto.ArticleListDto;
 import com.ssafy.api.dto.ArticlePostDto;
 import com.ssafy.api.service.ArticleService;
 import com.ssafy.api.service.S3UploaderService;
+import com.ssafy.core.entity.Photographer;
+import com.ssafy.core.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,23 +19,38 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("")
+@Controller
 public class ArticleController {
     @Autowired
     private ArticleService articleService;
-    private final S3UploaderService s3UploaderService;
+    @Autowired
+    private S3UploaderService s3UploaderService;
 
     @PostMapping("/api/article")
-    public Long uploadImage(@RequestPart(value="ArticlePostReq") ArticlePostDto articlePostDto,
-                            @RequestPart(value="image") List<MultipartFile> multipartFile) throws IOException {
+    public void uploadImage(@RequestPart(value="ArticlePostReq") ArticlePostDto articlePostDto,
+                            @RequestPart(value="image") List<MultipartFile> multipartFile,
+                            Photographer photographer) throws IOException {
         String fileName = s3UploaderService.upload(multipartFile);
+        articleService.postArticle(fileName, articlePostDto, photographer);
         System.out.println(fileName);
-        return articleService.postArticle(fileName, articlePostDto);
+    }
+
+    @GetMapping("/api/article/list/{photographer-id}")
+    @ResponseBody
+    public ArticleListDto getArticleList(@PathVariable("photographer-id") Long photographerId){
+        return articleService.getArticleList(photographerId);
     }
 
     @GetMapping("/api/article/{article-id}")
     @ResponseBody
     public ArticleDetailDto getArticleDetail(@PathVariable("article-id") Long articleId){
-        return articleService.findById(articleId);
+        return articleService.getArticleDetail(articleId);
+    }
+
+    @DeleteMapping("/api/article/{article-id}")
+    public void deleteArticle(@PathVariable("article-id") Long articleId,
+                                        User user){
+        articleService.DeletePost(articleId, user);
     }
 
 }
--- Smile_Server/api-module/src/main/java/com/ssafy/api/service/ArticleService.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/service/ArticleService.java
@@ -1,29 +1,67 @@
 package com.ssafy.api.service;
 
 import com.ssafy.api.dto.ArticleDetailDto;
+import com.ssafy.api.dto.ArticleListDto;
 import com.ssafy.api.dto.ArticlePostDto;
 import com.ssafy.core.entity.Article;
+import com.ssafy.core.entity.Photographer;
+import com.ssafy.core.entity.User;
+import com.ssafy.core.exception.CustomException;
+import com.ssafy.core.exception.ErrorCode;
 import com.ssafy.core.repository.ArticleRepository;
+import com.ssafy.core.repository.PhotographerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
 
 @Service
 public class ArticleService {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private S3UploaderService s3UploaderService;
+    @Autowired
+    private PhotographerRepository photographerRepository;
 
-    public Long postArticle(String fileName, ArticlePostDto articlePostDto){
+    public void postArticle(String fileName, ArticlePostDto articlePostDto, Photographer photographer){
         Article article = articlePostDto.toEntity();
         article.setPhotoUrls(fileName);
+        article.whoPost(photographer);
         articleRepository.save(article);
         System.out.println(article);
-        return article.getId();
     }
 
-    public ArticleDetailDto findById(Long id){
-        Article article = articleRepository.findById(id).orElseThrow(RuntimeException::new);
+    public ArticleDetailDto getArticleDetail(Long id){
+        Article article = articleRepository.findById(id).orElseThrow(()->new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
 
         return new ArticleDetailDto(article);
+    }
+
+    public void DeletePost(Long id, User User){
+        Article article = articleRepository.findById(id).orElseThrow(()-> new CustomException(ErrorCode.ARTICLE_NOT_FOUND));
+        if (article.getPhotographer().getId() == User.getId()){
+            String photoUriList = article.getPhotoUrls();
+            photoUriList.replace("[","").replace("]","");
+            List<String> photoUrls = new ArrayList<String>(Arrays.asList(photoUriList.split(",")));
+            photoUrls.forEach(new Consumer<String>() {
+                @Override
+                public void accept(String url) {
+                    s3UploaderService.deleteFile(url);
+                }
+            });
+
+            articleRepository.deleteById(id);
+        }
+    }
+
+    public ArticleListDto getArticleList(Long id){
+        Photographer photographer = photographerRepository.findById(id).orElseThrow(()->new CustomException(ErrorCode.PHOTOGRAPHER_NOT_FOUND));
+        ArticleListDto articleListDto = new ArticleListDto(photographer);
+        return articleListDto;
     }
 
 }
