2c99751adf13b3eff34492819e6506e9db2a2615
서재건 <sjk1052005@gmail.com>
2023-01-18 22:24:47 +0900

    [REFACTOR] #1 - Change findByEmail return type
    - orElseThrow() 처리하기 위해 리턴 타입 Optional로 수정
    
    
    Former-commit-id: 94fa468ab1d5652d95a9e5af97dd35df04ada9ae

--- Smile_Server/core-module/src/main/java/com/ssafy/core/repository/UserRepository.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/UserRepository.java
@@ -4,13 +4,16 @@ import com.ssafy.core.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  *
- * author 김정은
+ * author @김정은
+ * author @서재건
  */
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
+    public Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
 
