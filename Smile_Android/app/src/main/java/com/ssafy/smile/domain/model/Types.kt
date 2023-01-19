91e75e195d926e88dc33891416a91da1a6799ed0
minha721 <m0721@naver.com>
2023-01-19 13:31:35 +0900

    [FEAT] #11 - 포트폴리오 보기 enum data Types.kt로 옮김
    Former-commit-id: 037aff8eda35cb3459dfb24fbfb05f0d6d1a19ad

--- Smile_Android/app/src/main/java/com/ssafy/smile/domain/model/Types.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/domain/model/Types.kt
@@ -3,4 +3,5 @@ package com.ssafy.smile.domain.model
 object Types {
     enum class ToastType { ERROR, SUCCESS, INFO, WARNING, BASIC, CUSTOM}
     enum class ErrorType { NETWORK, TIMEOUT, UNKNOWN }
+    enum class Role { USER, PHOTOGRAPHER }
 }
