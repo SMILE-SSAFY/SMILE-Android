035bcf5b204956940746878c30519d6911997295
RingCho <marsm4rs@gmail.com>
2023-01-19 14:44:00 +0900

    [FIX] #13 - ArticleList By Photographer
    - 작가별 게시글 조회 구현 완료
    - 게시글 삭제 구현 완료
    - 게시글에 유저 정보 추가
    - 작성유저 == 요청유저 일 때만 삭제가능
    
    
    Former-commit-id: feb453c24d0188255dcfebe1681de24ab34dd102

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/ArticleListDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/ArticleListDto.java
@@ -1,26 +1,17 @@
 package com.ssafy.api.dto;
 
-import com.ssafy.core.entity.Article;
-import com.ssafy.core.entity.Photographer;
-import com.ssafy.core.entity.Places;
-import com.ssafy.core.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
+@Builder
+@AllArgsConstructor
 public class ArticleListDto {
-    private User user;
-    private Long photographerId;
-    private String name;
-    private String introduction;
-    private List<Places> places;
-    private List<Article> articles;
-
-    public ArticleListDto(Photographer photographer){
-        this.photographerId = photographer.getId();
-        this.name = user.getEmail();
-        this.introduction = photographer.getIntroduction();
-        this.places = photographer.getPlaces();
-    }
+    /***
+     * 작가별 게시글 조회에서 id 하나의 photoUrl만 돌려주기 위한 Dto
+     * {@code @author:} 신민철
+     */
+    private Long id;
+    private String photoUrl;
 }
--- Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleRepository.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.core.repository;
 
 import com.ssafy.core.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    List<Article> findByPhotographerId(Long id);
+    @Query(value = "SELECT * FROM board WHERE user_id = :userId ORDER BY id DESC", nativeQuery = true)
+    List<Article> findByUserId(Long userId);
 }
