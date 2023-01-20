f70ed234f1f9cc0bb9992264bee325ff6be8be66
minha <m0721@naver.com>
2023-01-20 10:33:44 +0900

    [FEAT] #1 - 회원가입 #41 - 유저로서, 회원가입 기능을 통해 회원등록을 한다
    Former-commit-id: 20892cf30b48347db3f2ab11b95fd06f0cf31a40

--- Smile_Android/app/src/main/java/com/ssafy/smile/common/util/Constants.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/common/util/Constants.kt
@@ -3,9 +3,9 @@ package com.ssafy.smile.common.util
 // For Constant Strings
 object Constants {
     // 정은
-    const val BASE_URL = "http://192.168.100.75:80"
+//    const val BASE_URL = "http://192.168.100.75:80"
     // 재건
-//    const val BASE_URL = "http://192.168.100.166:80"
+    const val BASE_URL = "http://192.168.100.166:80"
     // 민철
 //    const val BASE_URL = "http://192.168.100.164:80"
 }
