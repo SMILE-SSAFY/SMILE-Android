1d2eb42d621d3b729426ad4f880544da023bba7b
jiy00nlee <sundaeluv98@gmail.com>
2023-01-20 11:44:19 +0900

    [FIX] #2 Conflict with develop branch solve
    Former-commit-id: 3df01383969ef9fc41832b9d5a2233ad0d520163

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/LogInFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/LogInFragment.kt
@@ -1,6 +1,49 @@
 package com.ssafy.smile.presentation.view.user
 
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ssafy.smile.MainActivity
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentLogInBinding
+import com.ssafy.smile.domain.model.Types
+import com.ssafy.smile.presentation.base.BaseFragment
 
-class LogInFragment : Fragment() {
+class LogInFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::bind, R.layout.fragment_log_in) {
+
+    override fun initView() {
+        (activity as MainActivity).setToolBar(false, false, null)
+    }
+
+    override fun setEvent() {
+        binding.apply {
+            btnLogin.setOnClickListener {
+                if (checkValid()) {
+                    //TODO : 자동 로그인 처리
+                    //TODO : 로그인 서버통신 구현
+                    findNavController().navigate(R.id.action_logInFragment_to_mainFragment)
+                }
+            }
+
+            btnSignUp.setOnClickListener {
+                findNavController().navigate(R.id.action_logInFragment_to_signUp1Fragment)
+            }
+
+            btnSnsLogin.setOnClickListener {
+                //TODO : SNS 로그인 구현
+            }
+        }
+    }
+
+    private fun checkValid(): Boolean {
+        binding.apply {
+            return if (etId.text.toString().isEmpty()) {
+                showToast(requireContext(), "아이디를 입력해주세요", Types.ToastType.ERROR)
+                false
+            } else if (etPassword.text.toString().isEmpty()) {
+                showToast(requireContext(), "비밀번호를 입력해주세요", Types.ToastType.ERROR)
+                false
+            } else {
+                true
+            }
+        }
+    }
 }
