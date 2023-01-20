a7438653fe02f30cbb81af079ddde7b17cd11acf
서재건 <sjk1052005@gmail.com>
2023-01-20 14:26:49 +0900

    [FEAT] #1 - Feat get userDto
    - 유저 정보 조회
    - id, name, role
    
    
    Former-commit-id: e6e73e204dd30dd0616d77735fc184eb0715b3b2

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/User/UserDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/User/UserDto.java
@@ -1,4 +1,33 @@
 package com.ssafy.api.dto.User;
 
+import com.ssafy.core.code.Role;
+import com.ssafy.core.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserDto {
+    private Long id;
+    private String name;
+    private Role role;
+
+
+    /**
+     * User Entity에서 User Dto로 변경
+     *
+     * @param user
+     * @return 변환된 DTO
+     */
+    public UserDto of(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .role(user.getRole())
+                .build();
+    }
 }
