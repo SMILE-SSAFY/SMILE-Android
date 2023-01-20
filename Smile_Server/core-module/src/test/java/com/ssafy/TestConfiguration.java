f71aee3f80f9bbac59f4d53849377bbd657872de
서재건 <sjk1052005@gmail.com>
2023-01-20 10:09:19 +0900

    [FIX] #3 - Fix repositoryTest code Error in api-module
    - api-module에서 repository를 잡지 못함
    - core-module로 test 디렉토리 옮기고
    - main 실행 파일 추가
    - junit:4.13.1 추가
    
    
    Former-commit-id: 9d7a8f0667af061291c595e764f442711dbb7b94

---
+++ Smile_Server/core-module/src/test/java/com/ssafy/TestConfiguration.java
@@ -0,0 +1,11 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConfiguration {
+    public static void main(String[] args) {
+        SpringApplication.run(TestConfiguration.class, args);
+    }
+}
