6652ce3461551b62a41a3c05b5ecd7c5feb2e37d
jiy00nlee <sundaeluv98@gmail.com>
2023-01-22 02:23:15 +0900

    [FEAT] #8 - 마이페이지 UI 추가
    Former-commit-id: 365c3734c8856fd5c38b88ea007dd92aaf0ae76e

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/adapter/MainViewPagerAdapter.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/adapter/MainViewPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ssafy.smile.presentation.view.home.HomeFragment
 import com.ssafy.smile.presentation.view.map.MapFragment
-import com.ssafy.smile.presentation.view.mypage.MypageFragment
+import com.ssafy.smile.presentation.view.mypage.MyPageFragment
 
 class MainViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity){
     override fun getItemCount(): Int = 3
@@ -14,7 +14,7 @@ class MainViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(ac
         return when(position){
             0 -> MapFragment()
             1 -> HomeFragment()
-            2 -> MypageFragment()
+            2 -> MyPageFragment()
             else -> error("no such position: $position")
         }
     }
--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/mypage/MypageFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/mypage/MypageFragment.kt
@@ -1,21 +1,45 @@
 package com.ssafy.smile.presentation.view.mypage
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.ssafy.smile.databinding.FragmentMypageBinding
+import androidx.fragment.app.viewModels
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentMyPageBinding
+import com.ssafy.smile.presentation.base.BaseFragment
+import com.ssafy.smile.presentation.viewmodel.mypage.MyPageViewModel
 
-class MypageFragment : Fragment() {
-    private lateinit var viewbinding : FragmentMypageBinding
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(FragmentMyPageBinding::bind, R.layout.fragment_my_page) {
+    private val viewModel : MyPageViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        viewbinding = FragmentMypageBinding.inflate(inflater, container, false)
-        return viewbinding.root
+    override fun initView() {
+        setObserver()
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun setEvent() {
+        setClickListener()
+    }
+
+    private fun setObserver(){
+        viewModel.apply {
+
+        }
+    }
+
+    private fun setClickListener(){
+        binding.apply {
+            layoutMyPageProfile.apply {  }
+            layoutMyPageUserDetails.apply {
+                tvCustomerReservation.setOnClickListener{ }
+                tvCustomerInterest.setOnClickListener { }
+            }
+            layoutMyPagePhotographer.apply {
+                clPhotographerRegister.setOnClickListener {  }
+                tvPhotographerReservation.setOnClickListener {  }
+                tvPhotographerPortfolio.setOnClickListener {  }
+                tvPhotographerMonthTax.setOnClickListener {  }
+            }
+            layoutMyPageUser.apply {
+                tvUserChangePassword.setOnClickListener {  }
+                tvUserWithDraw.setOnClickListener {  }
+            }
+        }
     }
 }
