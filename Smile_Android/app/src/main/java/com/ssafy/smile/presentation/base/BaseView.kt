c7eda08bdf1c212cd0fd6a1b0dcaa119f4cb28bd
jiy00nlee <sundaeluv98@gmail.com>
2023-01-22 12:10:24 +0900

    [FEAT] #8 - Toolbar 공통 코드 작성
    Former-commit-id: 8eb411e5772ce7f7643008112fad0c0e7d48181d

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/base/BaseView.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/base/BaseView.kt
@@ -2,13 +2,12 @@ package com.ssafy.smile.presentation.base
 
 import android.app.Dialog
 import android.content.Context
-import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.LifecycleOwner
 import com.ssafy.smile.domain.model.Types
-import es.dmoral.toasty.Toasty
 
 interface BaseView {
+    fun Toolbar.initToolbar(toolbarTitle:String, isBackAvailable:Boolean?=false, backAction : (() -> Unit)?=null )
     fun showLoadingDialog(context: Context){}
     fun dismissLoadingDialog(){}
     fun showDialog(dialog: Dialog, lifecycleOwner: LifecycleOwner?, cancelable: Boolean = true, dismissHandler: (() -> Unit)? = null){}
