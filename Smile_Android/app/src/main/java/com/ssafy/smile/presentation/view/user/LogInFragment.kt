eab740446301ce15a6acf79bb6432767e67ce7bf
이민하 <m0721@naver.com>
2023-01-25 23:45:30 +0900

    [FEAT] #3 - 소셜로그인(카카오) #92 - 카카오 로그인 구현
    Former-commit-id: aaa37df185b2ccd49955924e3cc596b0fe4e9c30

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/LogInFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/user/LogInFragment.kt
@@ -1,7 +1,12 @@
 package com.ssafy.smile.presentation.view.user
 
+import android.util.Log
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.ssafy.smile.MainActivity
 import com.ssafy.smile.R
 import com.ssafy.smile.common.util.NetworkUtils
@@ -16,6 +21,12 @@ private const val TAG = "LogInFragment_스마일"
 class LogInFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::bind, R.layout.fragment_log_in) {
 
     private val userViewModel by activityViewModels<UserViewModel>()
+
+    private val callback: (OAuthToken?, Throwable?) -> Unit = { _, error ->
+        if (error != null) {
+            Log.d(TAG, "카카오계정으로 로그인 실패", error)
+        }
+    }
 
     override fun initView() {
         (activity as MainActivity).setToolBar(isUsed = false, isBackUsed = false, title = null)
@@ -35,7 +46,7 @@ class LogInFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::b
             }
 
             btnSnsLogin.setOnClickListener {
-                //TODO : SNS 로그인 구현
+                kakaoLogin()
             }
         }
         loginResponseObserver()
@@ -79,6 +90,29 @@ class LogInFragment : BaseFragment<FragmentLogInBinding>(FragmentLogInBinding::b
     private fun getLoginInfo(): LoginDomainDto {
         binding.apply {
             return LoginDomainDto(etId.text.toString(), etPassword.text.toString())
+        }
+    }
+
+    private fun kakaoLogin() {
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(requireContext())) {
+            UserApiClient.instance.loginWithKakaoTalk(requireContext()) { token, error ->
+                if (error != null) {
+                    Log.d(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
+                } else if (token != null) {
+                    //TODO : 카카오 로그인 서버 통신 구현하기 -> token.accessToken
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(requireContext(), callback = callback)
         }
     }
 }
