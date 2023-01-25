b1249c735790f4321639b95151d757fffaaaa656
jungeun <jegarnet23@gmail.com>
2023-01-25 22:13:32 +0900

    [FEAT] #8 - Create Places table
    - 활동지역 테이블 생성
    - 활동지역 연관관계 테이블 생성
    
    
    Former-commit-id: b0f1f7e8ec100a8addfeaa91a41efd4aa3f2df43

---
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/entity/PhotographerNPlaces.java
@@ -0,0 +1,42 @@
+package com.ssafy.core.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "photographer_places")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PhotographerNPlaces {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "photographer_id", referencedColumnName = "id")
+    private Photographer photographer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id", referencedColumnName = "id")
+    private Places places;
+
+    public void updatePhotographer(Long photographerId){
+        this.photographer = Photographer.builder()
+                .id(photographerId)
+                .build();
+    }
+}
---
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/PhotographerNPlacesRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.core.repository;
+
+import com.ssafy.core.entity.PhotographerNPlaces;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotographerNPlacesRepository extends JpaRepository<PhotographerNPlaces, Long> {
+}
