f541aea7fa798509843db4530dd01595c9ff425e
minha <m0721@naver.com>
2023-01-27 09:41:23 +0900

    [FEAT] #18 - 게시글 카테고리 검색 게시글 검색 리사이클러뷰 구현, 게시글 검색 좋아요 구현
    Former-commit-id: 25028d2c2ae16f79b8e7104bdcd229dc38d9ea47

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/home/ResultPostFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/home/ResultPostFragment.kt
@@ -1,15 +1,52 @@
 package com.ssafy.smile.presentation.view.home
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ssafy.smile.R
 import com.ssafy.smile.databinding.FragmentResultPostBinding
+import com.ssafy.smile.domain.model.CustomPostDomainDto
+import com.ssafy.smile.presentation.adapter.ResultPostRecyclerAdapter
 import com.ssafy.smile.presentation.base.BaseFragment
 
 class ResultPostFragment : BaseFragment<FragmentResultPostBinding>(FragmentResultPostBinding::bind, R.layout.fragment_result_post) {
+    private lateinit var resultPostRecyclerAdapter: ResultPostRecyclerAdapter
+    private val recyclerData = mutableListOf<CustomPostDomainDto>()
+
     override fun initView() {
-//        TODO("Not yet implemented")
+        for (i in 1..10) {
+            recyclerData.add(
+                CustomPostDomainDto(
+                    "https://edu.ssafy.com/asset/images/header-logo.jpg",
+                    "싸피",
+                    "싸피",
+                    "싸피",
+                    false,
+                    100
+                )
+            )
+            recyclerData.add(
+                CustomPostDomainDto(
+                    "https://edu.ssafy.com/asset/images/header-logo.jpg",
+                    "싸피",
+                    "싸피",
+                    "싸피",
+                    true,
+                    100
+                )
+            )
+        }
+        initRecycler()
     }
 
     override fun setEvent() {
 //        TODO("Not yet implemented")
+    }
+
+    private fun initRecycler() {
+        resultPostRecyclerAdapter = ResultPostRecyclerAdapter(requireContext(), recyclerData)
+
+        binding.rvPhotographerResult.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = resultPostRecyclerAdapter
+        }
     }
 }
