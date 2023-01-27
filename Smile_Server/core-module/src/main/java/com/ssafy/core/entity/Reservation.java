57e9fbe49bfe191ff27d296a55a813ebcc1f12c5
jungeun <jegarnet23@gmail.com>
2023-01-27 16:48:01 +0900

    [CREATE] #21 - Create Reservation Entity
    - 예약 테이블 생성
    
    
    Former-commit-id: 854d718634ea857c945ab5c36859cef416c39c29

---
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/entity/Reservation.java
@@ -0,0 +1,55 @@
+package com.ssafy.core.entity;
+
+import com.ssafy.core.code.ReservationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.sql.Date;
+import java.sql.Time;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "reservation")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id", insertable = false, updatable = false)
+    private Photographer photographer;
+
+    @ManyToOne
+    @JoinColumn(name = "id", insertable = false, updatable = false)
+    private User user;
+
+    @ColumnDefault("0")
+    @Enumerated(EnumType.ORDINAL)
+    @Builder.Default
+    private ReservationStatus status = ReservationStatus.예약확정전;
+
+    private int price;
+
+    private Date date;
+
+    private Time time;
+
+    @Column(name = "reviewed")
+    @ColumnDefault("false")
+    private boolean isReviewed;
+}
