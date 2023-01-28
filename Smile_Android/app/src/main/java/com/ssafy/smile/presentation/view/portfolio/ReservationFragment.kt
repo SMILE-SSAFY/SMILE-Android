754c14363a3b628c1871dcb253e95c5c8011a400
이민하 <m0721@naver.com>
2023-01-28 17:52:22 +0900

    [FEAT] #21 - 예약 예약하기 뷰 구현
    Former-commit-id: 35257bb95ff46a76af5c8dadf26f1680ecaa70e5

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
@@ -1,6 +1,21 @@
 package com.ssafy.smile.presentation.view.portfolio
 
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
+import com.ssafy.smile.R
+import com.ssafy.smile.databinding.FragmentReservationBinding
+import com.ssafy.smile.presentation.base.BaseFragment
 
-class ReservationFragment : Fragment() {
+class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentReservationBinding::bind, R.layout.fragment_reservation) {
+    override fun initView() {
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar : Toolbar = binding.layoutToolbar.tbToolbar
+        toolbar.initToolbar("예약하기", true)
+    }
+
+    override fun setEvent() {
+//        TODO("Not yet implemented")
+    }
 }
