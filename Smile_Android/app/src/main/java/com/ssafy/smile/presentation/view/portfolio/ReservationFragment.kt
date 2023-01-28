1d85f091f6d1e2689e04ae2b9d448305065c0f48
이민하 <m0721@naver.com>
2023-01-28 18:39:50 +0900

    [FEAT] #21 - 예약 시작 시간 다이얼로그 구현
    Former-commit-id: 1f733e05965d1d5c92a83c5aeaa6eca3dbc77d26

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
@@ -1,11 +1,18 @@
 package com.ssafy.smile.presentation.view.portfolio
 
+import android.graphics.Color
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_KEYBOARD
+import com.google.android.material.timepicker.TimeFormat
 import com.ssafy.smile.R
 import com.ssafy.smile.databinding.FragmentReservationBinding
 import com.ssafy.smile.presentation.base.BaseFragment
 
 class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentReservationBinding::bind, R.layout.fragment_reservation) {
+
+    var isTimeChecked = false
+
     override fun initView() {
         initToolbar()
     }
@@ -16,6 +23,41 @@ class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentRes
     }
 
     override fun setEvent() {
-//        TODO("Not yet implemented")
+        binding.apply {
+            btnTime.setOnClickListener {
+                showTimePicker()
+            }
+        }
+    }
+
+    private fun showTimePicker() {
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setInputMode(INPUT_MODE_KEYBOARD)
+            .setPositiveButtonText("선택")
+            .setTitleText("시작 시간을 선택하세요")
+            .build()
+
+        timePicker.addOnPositiveButtonClickListener {
+            setTimeText(timePicker.hour, timePicker.minute)
+            isTimeChecked = true
+        }
+
+        timePicker.show(requireFragmentManager(), "time picker tag")
+    }
+
+    private fun setTimeText(hour: Int, minute: Int) {
+        val ampm = if (hour > 12) {
+            "오후"
+        } else {
+            "오전"
+        }
+        val hour = String.format("%02d", hour % 12)
+        val min = String.format("%02d", minute)
+
+        binding.apply {
+            tvTime.setTextColor(Color.BLACK)
+            tvTime.text = "${ampm} ${hour}시 ${min}분"
+        }
     }
 }
