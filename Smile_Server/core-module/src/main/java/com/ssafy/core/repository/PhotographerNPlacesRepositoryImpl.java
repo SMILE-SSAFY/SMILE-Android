9f07ebb27c2a623dac3333f4866d7f59300f7203
서재건 <sjk1052005@gmail.com>
2023-01-28 18:32:49 +0900

    [FEAT] #19 - 주변 작가 목록 조회 querydsl 작성
    - 주변 작가 목록 조회에 사용하는 querydsl 작성
    - querydsl에서 사용할 dto생성 위해 core-module에 dto 폴더 생성
    
    
    Former-commit-id: d9ca34bafde2009ef3a1b8143e6933dcdf71c1ef

---
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/PhotographerNPlacesRepositoryImpl.java
@@ -0,0 +1,61 @@
+package com.ssafy.core.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.core.dto.PhotographerQuerydslDto;
+import com.ssafy.core.entity.QPhotographer;
+import com.ssafy.core.entity.QPhotographerHeart;
+import com.ssafy.core.entity.QPhotographerNPlaces;
+import com.ssafy.core.entity.QPlaces;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 사진 작가와 활동 지역 Repository Querydsl Impl
+ *
+ * author @서재건
+ */
+@RequiredArgsConstructor
+public class PhotographerNPlacesRepositoryImpl implements PhotographerNPlacesRepositoryCustom{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    /**
+     * 활동지역에 해당하는 사진작가 조회
+     *
+     * @param first
+     * @param second
+     * @return List<Photographer>
+     */
+    @Override
+    public List<PhotographerQuerydslDto> findPhotographerByAddress(Long userId, String first, String second) {
+        QPhotographerNPlaces photographerNPlaces = QPhotographerNPlaces.photographerNPlaces;
+        QPlaces places = QPlaces.places;
+        QPhotographer photographer = QPhotographer.photographer;
+        QPhotographerHeart photographerHeart = QPhotographerHeart.photographerHeart;
+
+        return jpaQueryFactory
+                .select(Projections.constructor(PhotographerQuerydslDto.class,
+                        photographer,
+                        photographerHeart.id.count(),
+                        new CaseBuilder()
+                                .when(photographer.id.in(
+                                        JPAExpressions
+                                                .select(photographerHeart.photographer.id)
+                                                .from(photographerHeart)
+                                                .where(photographerHeart.user.id.eq(userId))
+                                )).then(true)
+                                .otherwise(false)
+                        ))
+                .from(photographer)
+                .leftJoin(photographerHeart).on(photographer.eq(photographerHeart.photographer))
+                .join(photographerNPlaces).on(photographer.eq(photographerNPlaces.photographer))
+                .join(places).on(places.eq(photographerNPlaces.places))
+                .where(places.first.eq(first), places.second.eq(second))
+                .groupBy(photographer.id)
+                .fetch();
+    }
+}
