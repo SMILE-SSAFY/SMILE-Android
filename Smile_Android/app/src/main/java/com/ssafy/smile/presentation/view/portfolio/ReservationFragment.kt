0c37e39a6590cf00e057cf6ca92353b22c46c80e
이민하 <m0721@naver.com>
2023-01-28 18:54:29 +0900

    [FEAT] #21 - 예약 이메일 확인 다이얼로그 구현
    Former-commit-id: 4bc54bc03029b5cbb6c67c6f6f9fcd6768ad5a9f

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/ReservationFragment.kt
@@ -6,12 +6,17 @@ import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.MaterialTimePicker.INPUT_MODE_KEYBOARD
 import com.google.android.material.timepicker.TimeFormat
 import com.ssafy.smile.R
+import com.ssafy.smile.common.view.CommonDialog
 import com.ssafy.smile.databinding.FragmentReservationBinding
+import com.ssafy.smile.domain.model.DialogBody
 import com.ssafy.smile.presentation.base.BaseFragment
 
 class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentReservationBinding::bind, R.layout.fragment_reservation) {
 
-    var isTimeChecked = false
+    var isDateChecked = true
+    var isTimeChecked = true
+    var isPlaceChecked = true
+    var isCategoryChecked = true
 
     override fun initView() {
         initToolbar()
@@ -24,8 +29,16 @@ class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentRes
 
     override fun setEvent() {
         binding.apply {
+            btnDate.setOnClickListener {
+                //TODO : 데이트 피커 띄우기
+            }
             btnTime.setOnClickListener {
                 showTimePicker()
+            }
+            btnPay.setOnClickListener {
+                if (isAllInput()) {
+                    showEmailCheckDialog()
+                }
             }
         }
     }
@@ -58,6 +71,22 @@ class ReservationFragment : BaseFragment<FragmentReservationBinding>(FragmentRes
         binding.apply {
             tvTime.setTextColor(Color.BLACK)
             tvTime.text = "${ampm} ${hour}시 ${min}분"
+        }
+    }
+
+    private fun isAllInput(): Boolean {
+        return isDateChecked && isTimeChecked && isPlaceChecked && isCategoryChecked
+    }
+
+    private fun showEmailCheckDialog() {
+        binding.apply {
+            val dialog = CommonDialog(
+                requireContext(),
+                DialogBody("잠깐!\n사진을 전송 받을 이메일이 맞나요?\n\n${etEmail.text}", "맞아요", "틀려요"),
+                { showToast(requireContext(), "결제 페이지로 이동~") },
+                { showToast(requireContext(), "이메일을 다시 입력해주세요") }
+            )
+            showDialog(dialog, viewLifecycleOwner)
         }
     }
 }
