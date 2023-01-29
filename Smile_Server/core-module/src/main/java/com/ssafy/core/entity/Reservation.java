d55ec083a15835faf93437b9ee904cbe7b4cf147
jungeun <kjjee99@naver.com>
2023-01-29 23:51:48 +0900

    [ADD] #21 - Add email column and change date and time column
    - 이메일 컬럼 추가
    - date & time -> reserved_at & reserved_time
    
    
    Former-commit-id: 99eea87d718cf949b93c5dd10a914d30a9c657c7

--- Smile_Server/core-module/src/main/java/com/ssafy/core/entity/Reservation.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/entity/Reservation.java
@@ -7,16 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDateTime;
@@ -33,12 +24,12 @@ public class Reservation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "id", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "photographer_id")
     private Photographer photographer;
 
-    @ManyToOne
-    @JoinColumn(name = "id", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ColumnDefault("0")
@@ -48,12 +39,14 @@ public class Reservation {
 
     private int price;
 
+    private String email;
+
     // 만나는 장소
     private String place;
 
-    private Date date;
+    private Date reservedAt;
 
-    private Time time;
+    private Time reservedTime;
 
     @Column(name = "reviewed")
     @ColumnDefault("false")
