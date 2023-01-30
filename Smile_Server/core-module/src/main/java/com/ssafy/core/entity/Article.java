a7f88497410e096d9664506e3efc56652fced6f7
JG서 <sjk1052005@gmail.com>
2023-01-30 23:01:34 +0900

    [ADD] #26 - Add hearts column
    - 엔티티에 좋아요 컬럼 추가
    
    
    Former-commit-id: 45ab9d26d3af2e12d06bb6706dc57f0b101ed630

--- Smile_Server/core-module/src/main/java/com/ssafy/core/entity/Article.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/entity/Article.java
@@ -2,14 +2,29 @@ package com.ssafy.core.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
-import java.time.LocalDateTime;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 게시글 Entity
+ *
+ * @author 신민철
+ * @author 서재건
+ */
 @Entity
 @NoArgsConstructor
-@Data
+@Getter
 @Builder
 @AllArgsConstructor
 public class Article {
@@ -26,13 +41,14 @@ public class Article {
 
     private String detailAddress;
 
-//    private Integer heart;
-
     private LocalDateTime createdAt;
 
     private String category;
 
     private String photoUrls;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    private List<ArticleHeart> hearts;
 
     public void whoPost(User user){
         this.user = user;
