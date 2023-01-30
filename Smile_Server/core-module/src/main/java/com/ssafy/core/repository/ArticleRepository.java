240cd28c0375be195294c4fab31039676c7c3c3b
서재건 <sjk1052005@gmail.com>
2023-01-30 09:51:25 +0900

    [FEAT] #18 - search article by CategoryName
    - categoryName 으로 전달 받아서 카테고리 검색 후 게시글 검색
    
    
    Former-commit-id: 0447ccce47547e2a8c1f0908f5c4c8b5ca4aea35

--- Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleRepository.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleRepository.java
@@ -2,11 +2,16 @@ package com.ssafy.core.repository;
 
 import com.ssafy.core.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+/**
+ * 게시글 repository
+ *
+ * author @신민철
+ * author @서재건
+ */
+public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
     List<Article> findByUserId(Long user_id);
     List<Article> findAllByLatitudeBetweenAndLongitudeBetween(Double y1, Double y2, Double x1, Double x2);
 }
