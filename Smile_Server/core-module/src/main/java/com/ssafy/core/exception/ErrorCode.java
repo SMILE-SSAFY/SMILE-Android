c9f39121b80913ae451281ffa2305e57e527e347
RingCho <marsm4rs@gmail.com>
2023-01-30 17:58:40 +0900

    [REFACTOR] #14, #15 - 게시글 삭제, 수정시 본인이 아닐 때 에러 반환
    Former-commit-id: 8bfce69a466085c1c9b62a6c54e0d65dec23e613

--- Smile_Server/core-module/src/main/java/com/ssafy/core/exception/ErrorCode.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/exception/ErrorCode.java
@@ -25,6 +25,8 @@ public enum ErrorCode {
     FAIL_AUTHORIZATION(HttpStatus.FORBIDDEN, "권한이 없는 요청입니다."),
     NO_IMAGE(HttpStatus.BAD_REQUEST, "이미지가 존재하지 않습니다."),
     HAS_PHONENUMBER(HttpStatus.BAD_REQUEST, "이미 등록된 휴대폰 번호입니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 카테고리는 존재하지 않습니다."),
+    USER_MISMATCH(HttpStatus.FORBIDDEN, "해당 게시글에 접근할 권한이 없는 유저 입니다.")
     ;
 
 
