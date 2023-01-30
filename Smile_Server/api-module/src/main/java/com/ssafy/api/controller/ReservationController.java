6e119b7e7159d2273e5b61da7e78cef9f70e56cc
jungeun <jegarnet23@gmail.com>
2023-01-30 16:18:04 +0900

    [REFACTOR] #21 - Finish function getPhotographerInfo
    - 예약 시 사진작가 별 정보 얻어오는 함수 구현
    
    
    Former-commit-id: 18131b60e31e192be7d3c65926a54a289659c082

--- Smile_Server/api-module/src/main/java/com/ssafy/api/controller/ReservationController.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/controller/ReservationController.java
@@ -1,11 +1,10 @@
 package com.ssafy.api.controller;
 
-import com.ssafy.api.dto.Reservation.ReservationDto;
+import com.ssafy.api.dto.Reservation.ReservationReqDto;
 import com.ssafy.api.service.ReservationService;
 import com.ssafy.core.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,14 +30,13 @@ public class ReservationController {
      * @return 정상일 때 OK
      */
     @PostMapping
-    public ResponseEntity<?> registerReservation(@RequestBody ReservationDto reservation){
+    public ResponseEntity<?> registerReservation(@RequestBody ReservationReqDto reservation){
         log.info(reservation.toString());
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = (User)authentication.getPrincipal();
         reservation.setUserId(user.getId());
 
-        reservationService.reserve(reservation);
-        return ResponseEntity.ok(HttpStatus.OK);
+        return ResponseEntity.ok(reservationService.reserve(reservation));
     }
 
     @GetMapping("/{photographerId}")
