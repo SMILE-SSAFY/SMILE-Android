4f66864c1ed4e34633d01c144171bed93e987dd1
RingCho <marsm4rs@gmail.com>
2023-01-31 16:29:19 +0900

    [CHORE] JAVA DOC 수정 및 추가
    Former-commit-id: 1bb1e42cc4ec394e0a63a8377d4a0a945f2b854a

--- Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleClusterRepository.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/ArticleClusterRepository.java
@@ -6,6 +6,12 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
+/***
+ * 클러스터링을 위한 repo
+ * redis에 저장
+ *
+ * @author 신민철
+ */
 public interface ArticleClusterRepository extends CrudRepository<ArticleCluster, Long> {
     Optional<ArticleCluster> findById(Long Id);
 }
