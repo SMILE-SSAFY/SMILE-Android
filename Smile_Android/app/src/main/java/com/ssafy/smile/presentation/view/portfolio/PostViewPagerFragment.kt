312233e132fa80c66c2b73561fcaa01f03d89d15
minha <m0721@naver.com>
2023-02-01 14:14:15 +0900

    [FEAT] #27 - 예약 내역 확인(작가) 예약 내역 뷰 구현, 커스텀뷰 버튼 추가
    Former-commit-id: 6aafeb42b01f9bab1321799a0ca15a14eacbc701

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/PostViewPagerFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/portfolio/PostViewPagerFragment.kt
@@ -24,7 +24,6 @@ class PostViewPagerFragment : BaseFragment<FragmentPostViewPagerBinding>(Fragmen
     private val recyclerData = mutableListOf<PostListResponseDto>()
 
     override fun initView() {
-        portfolioViewModel.getPosts(PortfolioFragment().photographerId)
         setObserver()
         initRecycler()
     }
