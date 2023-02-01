4c4287b73bcb14e474d8d16749231f4470250498
jungeun <jegarnet23@gmail.com>
2023-02-01 09:14:24 +0900

    [ADD] #22 - Add receiptId parameter
    Former-commit-id: 7d0d90b407ed10beb0ed2c1cd776ed2300a16688

--- Smile_Server/api-module/src/main/java/com/ssafy/api/controller/PayController.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/controller/PayController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +17,8 @@ public class PayController {
     private PayService payService;
 
     @GetMapping("/api/pay")
-    public ResponseEntity<?> confirmPay(){
-        payService.confirmPay();
+    public ResponseEntity<?> confirmPay(@RequestParam("receiptId") String receiptId){
+        payService.confirmPay(receiptId);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
