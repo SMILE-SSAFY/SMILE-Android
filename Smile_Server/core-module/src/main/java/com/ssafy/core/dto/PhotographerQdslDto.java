1e75ad719380494fc95ddc6146ea466f91c9c979
서재건 <sjk1052005@gmail.com>
2023-02-03 15:10:37 +0900

    [FEAT] #16 - Add order by near Photographer List
    - 주변 작가 목록 조회에서 정렬 기능 추가
    - 정렬 좋아요, 평점, 리뷰 수, 기본 id 순
    - photographerQdslDto, PhotographerForListDto 평점, 리뷰 수 필드 추가
    - 내가 찜한 작가 순
    
    
    Former-commit-id: 3511aaabeb08f9e33b06098509aad336e4c2b8b5

--- Smile_Server/core-module/src/main/java/com/ssafy/core/dto/PhotographerQdslDto.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/dto/PhotographerQdslDto.java
@@ -6,12 +6,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * querydsl 매핑 dto
+ *
+ * @author 서재건
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhotographerQdslDto {
     private Photographer photographer;
+    private double score;
     private Long heart;
+    private Long reviews;
     private boolean hasHeart;
 }
