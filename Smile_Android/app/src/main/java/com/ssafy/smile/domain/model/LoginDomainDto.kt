3ec74b1798d6193960ca59e7c70c5e7794c5cadb
이민하 <m0721@naver.com>
2023-02-03 22:23:32 +0900

    [HOTFIX] #AOS - 회원가입, 로그인, 소셜로그인 시 request body에 FCM Token 포함
    Former-commit-id: afdb0a76c999bed7434f060cf005c5b0ef32b1c8

--- Smile_Android/app/src/main/java/com/ssafy/smile/domain/model/LoginDomainDto.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/domain/model/LoginDomainDto.kt
@@ -4,7 +4,8 @@ import com.ssafy.smile.data.remote.model.LoginRequestDto
 
 data class LoginDomainDto(
     val email: String = "",
-    val password: String = ""
+    val password: String = "",
+    val fcmToken: String = ""
 ){
-    fun toLoginRequestDto(): LoginRequestDto = LoginRequestDto(email, password)
+    fun toLoginRequestDto(): LoginRequestDto = LoginRequestDto(email, password, fcmToken)
 }
