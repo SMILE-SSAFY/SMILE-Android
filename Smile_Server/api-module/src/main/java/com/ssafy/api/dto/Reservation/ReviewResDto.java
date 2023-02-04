fd98f76c68da2ae0668d073451e5eda2f98ffecb
RingCho <marsm4rs@gmail.com>
2023-02-04 19:56:54 +0900

    [ADD] #29 - 리뷰 createdAt 추가 리뷰조회시 stack overflow해결
    Former-commit-id: 5e0a3c308ceb0ad0c4e212c2fdd7c2e84e312fae

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/Reservation/ReviewResDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/Reservation/ReviewResDto.java
@@ -3,6 +3,8 @@ package com.ssafy.api.dto.Reservation;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 /***
  * 모든 리뷰조회를 위한 dto
  * @author 신민철
@@ -22,6 +24,8 @@ public class ReviewResDto {
     private String content;
 
     private String photoUrl;
+
+    private LocalDateTime createdAt;
 
     private Boolean isMe;
 }
