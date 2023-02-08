24d246607c71484dde57c4fe0395368ef0f4d718
이민하 <m0721@naver.com>
2023-02-08 22:53:14 +0900

    [HOTFIX] #AOS - 네트워크 에러 해결
    Former-commit-id: 13ed380aa2ad9ba4ee51baa14a2d450d4b171543

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/splash/SplashFragment.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/view/splash/SplashFragment.kt
@@ -1,6 +1,5 @@
 package com.ssafy.smile.presentation.view.splash
 
-import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.navigation.fragment.findNavController
@@ -8,52 +7,45 @@ import com.ssafy.smile.R
 import com.ssafy.smile.common.util.NetworkConnection
 import com.ssafy.smile.common.util.SharedPreferencesUtil
 import com.ssafy.smile.databinding.FragmentSplashBinding
-import com.ssafy.smile.domain.model.Types
 import com.ssafy.smile.presentation.base.BaseFragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.math.abs
 
+private const val TAG = "SplashFragment_스마일"
 class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::bind, R.layout.fragment_splash) {
 
     var isTokenValid = false
+    var isFirstCheck = true
 
     override fun initView() {
         checkToken()
     }
 
     override fun setEvent() {
-        val scope = CoroutineScope(Dispatchers.Main)
+        NetworkConnection(requireContext()).observe(viewLifecycleOwner) {
+            CoroutineScope(Dispatchers.Main).launch {
+                if (isFirstCheck) {
+                    delay(3000)
+                    val splashAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.animation_splash)
+                    binding.apply {
+                        ivLogo.visibility = View.VISIBLE
+                        binding.ivLogo.startAnimation(splashAnim)
+                    }
+                    delay(1200)
+                    isFirstCheck = false
+                }
 
-        scope.launch {
-            delay(3000)
-            val splashAnim = AnimationUtils.loadAnimation(requireContext(), R.anim.animation_splash)
-            binding.apply {
-                ivLogo.visibility = View.VISIBLE
-                binding.ivLogo.startAnimation(splashAnim)
-            }
-
-            delay(1200)
-
-//            val connection = NetworkConnection(requireContext())
-//            connection.observe(viewLifecycleOwner) {
-//                Log.d("스마일", "setEvent: ${it}")
-//                if (it) {
-//                    if (isTokenValid) {
-//                        findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
-//                    } else {
-//                        findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
-//                    }
-//                } else {
-//                    showToast(requireContext(), "네트워크 상태를 체크해주세요.")
-//                }
-//            }
-            if (isTokenValid) {
-                findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
-            } else {
-                findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
+                if (it) {
+                    withContext(Dispatchers.Main) {
+                        if (isTokenValid) {
+                            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
+                        } else {
+                            findNavController().navigate(R.id.action_splashFragment_to_logInFragment)
+                        }
+                    }
+                } else {
+                    showToast(requireContext(), "네트워크 상태를 확인해주세요")
+                }
             }
         }
     }
