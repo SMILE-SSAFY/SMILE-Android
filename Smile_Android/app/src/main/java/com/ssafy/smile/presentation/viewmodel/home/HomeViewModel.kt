ee8a21be628d8c1a8c9181b0f68ba58b022a2f35
jiy00nlee <sundaeluv98@gmail.com>
2023-02-10 00:11:48 +0900

    [HOTFIX] #AOS - 게시글 수정 로직 완료
    Former-commit-id: b1b309a035af0cbd675e5b21424c3a5e38c40c52

--- Smile_Android/app/src/main/java/com/ssafy/smile/presentation/viewmodel/home/HomeViewModel.kt
+++ Smile_Android/app/src/main/java/com/ssafy/smile/presentation/viewmodel/home/HomeViewModel.kt
@@ -34,7 +34,7 @@ class HomeViewModel: BaseViewModel() {
         heartRepository.photographerHeart(photographerId)
     }
 
-    fun getAddressList() = viewModelScope.launch(Dispatchers.IO) {
+    fun getCurretAddress() = viewModelScope.launch(Dispatchers.IO) {
         _getCurrentAddressResponse.postValue(addressRepository.getSelectedAddress())
     }
 }
