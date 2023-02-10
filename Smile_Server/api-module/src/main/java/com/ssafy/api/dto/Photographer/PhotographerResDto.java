e74804f2c7329dca586cff57bb4246ad7c2dd3e4
jungeun <jegarnet23@gmail.com>
2023-02-10 10:58:41 +0900

    [REFACTOR] 은행 column 추가
    Former-commit-id: 7cb11739d6c570c3bc424c079eed6868194e71c3

--- Smile_Server/api-module/src/main/java/com/ssafy/api/dto/Photographer/PhotographerResDto.java
+++ Smile_Server/api-module/src/main/java/com/ssafy/api/dto/Photographer/PhotographerResDto.java
@@ -25,6 +25,7 @@ public class PhotographerResDto {
     private String name;
     private String profileImg;
     private String introduction;
+    private String bank;
     private String account;
     private List<PlacesResDto> places;
     private List<CategoriesResDto> categories;
@@ -59,6 +60,7 @@ public class PhotographerResDto {
                 .name(photographer.getUser().getName())
                 .profileImg(photographer.getProfileImg())
                 .introduction(photographer.getIntroduction())
+                .bank(photographer.getBank())
                 .account(photographer.getAccount())
                 .places(places)
                 .categories(categories)
