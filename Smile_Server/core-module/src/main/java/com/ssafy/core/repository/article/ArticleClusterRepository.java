14dd2cd94d96b198fd3bb25b699ca6e0716f7cc9
RingCho <marsm4rs@gmail.com>
2023-02-15 14:16:53 +0900

    [FIX] 좋아요 게시글에서 눌러도 반영되도록 수정
    Former-commit-id: 50f9078a77e1024975c859d8e6e5e02b79e950a5

--- Smile_Server/core-module/src/main/java/com/ssafy/core/repository/article/ArticleClusterRepository.java
+++ Smile_Server/core-module/src/main/java/com/ssafy/core/repository/article/ArticleClusterRepository.java
@@ -4,6 +4,7 @@ import com.ssafy.core.entity.ArticleCluster;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 클러스터링 저장과 필터링 별 저장 및 조회를 위한 Repository
@@ -14,5 +15,6 @@ public interface ArticleClusterRepository extends CrudRepository<ArticleCluster,
     List<ArticleCluster> findAllByClusterIdAndUserIdOrderByArticleIdDesc(Long clusterId, Long userId);
     List<ArticleCluster> findAllByClusterIdAndUserIdOrderByHeartsDesc(Long clusterId, Long userId);
     List<ArticleCluster> findAllByClusterIdAndUserIdOrderByDistanceAsc(Long clusterId, Long userId);
+    Optional<ArticleCluster> findByArticleIdAndUserId(Long articleId, Long userId);
     void deleteAllByUserId(Long userId);
 }
